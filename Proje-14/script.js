b2bafbd401c48520bc5b25f7d013383dc0d41825
furkanelalmis <mardinlifurkan_1995@hotmail.com>
2022-02-10 18:22:58 +0300

    proje tamamlandı

--- Proje-14/script.js
+++ Proje-14/script.js
@@ -2,116 +2,121 @@
 
 let List = [];
 let editItem = {
-    id: ''
+  id: "",
 };
 
-
-
-
 function sec(id) {
-    return document.getElementById(id);
+  return document.getElementById(id);
 }
 
 function kayitOl() {
-    const name = sec("username").value;
-    const surname = sec("surname").value;
-    const email = sec("email").value;
-    const password = sec("password").value;
+  const name = sec("username").value;
+  const surname = sec("surname").value;
+  const email = sec("email").value;
+  const password = sec("password").value;
 
-    List.push({
-        id: List.length + 1,
-        isim: name,
-        soyisim: surname,
-        eposta: email,
-        sifre: password
-    });
-    getAll();
-    sec("formItem").reset();
+  List.push({
+    id: List.length + 1,
+    isim: name,
+    soyisim: surname,
+    eposta: email,
+    sifre: password,
+  });
+  getAll();
+  sec("formItem").reset();
 
-    // minlength minimum uzunluğu ister örnek olarak 5 karakterden az kullanamazsınız diye uyarı verir ! 
-    // required Giriş alanına bir öğe gerektirdiğini belirtir.
+  // minlength minimum uzunluğu ister örnek olarak 5 karakterden az kullanamazsınız diye uyarı verir !
+  // required Giriş alanına bir öğe gerektirdiğini belirtir.
 }
 
-
-
 function getAll() {
-    let html = ""; // html sayfasında tanımladığımız table'daki veriyi buraya cağırıyoruz
-    List.forEach(function(uye) { // uye değişkeni tanımladık 
-        html += "<tr>" +
-            "<th>" + uye.isim + "<th>" + // yukarıda fonksiyon içinde tanımlanan 'uye' yi push ettiğimiz değerler ile çağırırız.
-            "<th>" + uye.soyisim + "<th>" + // yukarıda fonksiyon içinde tanımlanan 'uye' yi push ettiğimiz değerler ile çağırırız.
-            "<th>" + uye.eposta + "<th>" + // yukarıda fonksiyon içinde tanımlanan 'uye' yi push ettiğimiz değerler ile çağırırız.
-            "<th>" + uye.sifre + "<th>" + // yukarıda fonksiyon içinde tanımlanan 'uye' yi push ettiğimiz değerler ile çağırırız.
-            "<td>" +
-            "<button class='btn btn-primary' onclick='elemanDüzenle(" + uye.id + " )' >Düzenle</button>" +
-            "<button class='btn btn-danger' onclick='elemanSil(" + uye.id + " )' >Sil</button>" +
-            "<td>" +
-            "<tr>";
-        // butonları burda tanımlayıp onclik özelliği verdik çünkü butonların tetiklemesi için 
-        // css özelliği verebiliyoruz butonlara burda 
-
-    })
-    sec("content").innerHTML = html;
-    // veriyi sayfaya basmak için tabloda id tanımladığımız yeri çağırıyoruz.
+  let html = ""; // html sayfasında tanımladığımız table'daki veriyi buraya cağırıyoruz
+  List.forEach(function (uye) {
+    // uye değişkeni tanımladık
+    html +=
+      "<tr>" +
+      "<th>" +
+      uye.isim +
+      "<th>" + // yukarıda fonksiyon içinde tanımlanan 'uye' yi push ettiğimiz değerler ile çağırırız.
+      "<th>" +
+      uye.soyisim +
+      "<th>" + // yukarıda fonksiyon içinde tanımlanan 'uye' yi push ettiğimiz değerler ile çağırırız.
+      "<th>" +
+      uye.eposta +
+      "<th>" + // yukarıda fonksiyon içinde tanımlanan 'uye' yi push ettiğimiz değerler ile çağırırız.
+      "<th>" +
+      uye.sifre +
+      "<th>" + // yukarıda fonksiyon içinde tanımlanan 'uye' yi push ettiğimiz değerler ile çağırırız.
+      "<td>" +
+      "<button class='btn btn-primary' onclick='elemanDüzenle(" +
+      uye.id +
+      " )' >Düzenle</button>" +
+      "<button class='btn btn-danger' onclick='elemanSil(" +
+      uye.id +
+      " )' >Sil</button>" +
+      "<td>" +
+      "<tr>";
+    // butonları burda tanımlayıp onclik özelliği verdik çünkü butonların tetiklemesi için
+    // css özelliği verebiliyoruz butonlara burda
+  });
+  sec("content").innerHTML = html;
+  // veriyi sayfaya basmak için tabloda id tanımladığımız yeri çağırıyoruz.
 }
 
 function duzenle() {
+  const duzen = List.findIndex((list) => list.id == editItem.id);
+  // findIndex kullandık : her dizi öğesi için bir işlev yürütür.
+  // findIndexin içine bir değişken tanımladık ve onu Id ile uyumlu hale getirdik.
+  List[duzen].isim = sec("username").value;
+  List[duzen].soyisim = sec("surname").value;
+  List[duzen].eposta = sec("email").value;
+  List[duzen].password = sec("password").value;
 
-    const duzen = List.findIndex(list => list.id == editItem.id);
-    // findIndex kullandık : her dizi öğesi için bir işlev yürütür.
-    // findIndexin içine bir değişken tanımladık ve onu Id ile uyumlu hale getirdik.
-    List[duzen].isim = sec("username").value
-    List[duzen].soyisim = sec("surname").value
-    List[duzen].eposta = sec("email").value
-    List[duzen].password = sec("password").value
+  // Push ettiğimiz değişkenleri burda tanımlıyoruz ve idlerini'de tanımlıyoruz.
 
-    // Push ettiğimiz değişkenleri burda tanımlıyoruz ve idlerini'de tanımlıyoruz.
+  getAll();
+  editItem = "";
+  sec("formItem").reset();
 
-    getAll();
-    editItem = "";
-    sec("formItem").reset();
-
-
-    // yukarıda tanımladğımız değişkeni burdada tanımlamamız gerekiyor yukarıda 'editItem' tanımladık burdada tanımlıyoruz.
+  // yukarıda tanımladğımız değişkeni burdada tanımlamamız gerekiyor yukarıda 'editItem' tanımladık burdada tanımlıyoruz.
 }
 
 function elemanDüzenle(elemanId) {
+  editItem = List.find((list) => list.id === elemanId);
+  // eleman düzenleyi onclik olarak tetiklediğimiz için burda tanımladık .
+  // find kullanmamızın sebebi her dizi öğesi için bir işlev yürütür.
+  // bir testi geçen ilk öğenin değerini döndürür.
+  // find elemanın kendisini verir
 
-    editItem = List.find(list => list.id === elemanId);
-    // eleman düzenleyi onclik olarak tetiklediğimiz için burda tanımladık .
-    // find kullanmamızın sebebi her dizi öğesi için bir işlev yürütür.
-    // bir testi geçen ilk öğenin değerini döndürür.
-    // find elemanın kendisini verir 
+  sec("username").value = editItem.isim;
+  sec("surname").value = editItem.soyisim;
+  sec("email").value = editItem.eposta;
+  sec("password").value = editItem.password;
 
-    sec("username").value = editItem.isim;
-    sec("surname").value = editItem.soyisim;
-    sec("email").value = editItem.eposta
-    sec("password").value = editItem.password;
-
-
-    // Veriyi sağdan alıp sola atmamız gerekiyor Çok önemli 
-
+  // Veriyi sağdan alıp sola atmamız gerekiyor Çok önemli
 }
 
 function elemanSil(elemanId) {
-    if (confirm("Elemanı Silmek İstediğinize Eminmisiniz ?")) {
-        const elemanıSil = List.findIndex(eleman => eleman.id == elemanId);
-        List.splice(elemanıSil, 1);
-        getAll();
-    }
-    // 'splice' metodu ile js'de dizilere eleman ekleyip silebiliyoruz.
-    // 'confirm' Elemanı gerçekten silmek istediğimizi teyit etmek için kullandık.
-    // fimdindex içerisnde bulunan parametrenin dizi içerisnde sırasını veren metoda fındındex denir 
+  if (confirm("Elemanı Silmek İstediğinize Eminmisiniz ?")) {
+    const elemanıSil = List.findIndex((eleman) => eleman.id == elemanId);
+    List.splice(elemanıSil, 1);
+    getAll();
+  }
+  // 'splice' metodu ile js'de dizilere eleman ekleyip silebiliyoruz.
+  // 'confirm' Elemanı gerçekten silmek istediğimizi teyit etmek için kullandık.
+  // fimdindex içerisnde bulunan parametrenin dizi içerisnde sırasını veren metoda fındındex denir
 }
 
-
-sec("formItem").addEventListener('submit', () => {
+sec("formItem").addEventListener(
+  "submit",
+  () => {
     if (parseInt(editItem.id) > 0) {
-        duzenle();
+      duzenle();
     } else {
-        kayitOl();
+      kayitOl();
     }
-
-}, false);
+  },
+  false
+);
 
 //
--- Proje-15/script.js
+++ Proje-15/script.js
@@ -1,26 +1,119 @@
 // Proje -15 Kitaplık Uygulaması
 // kitap bilgileri kitap hakkında bilgiler .
 
+let List = [];
+let editItem = {
+  id: "",
+};
+
 function sec(id) {
-    return document.getElementById(id);
+  return document.getElementById(id);
 }
 
 function ekle() {
-    const bookname = sec("bookName").value; // Kitap adı 
-    const authorname = sec("authorName").value; // Yazar adı
-    const booksubject = sec("bookSubject").value; // Kitap Konusu
-    const publicationyear = sec("publicationYear").value; // Kitap Basım Yılı 
-    const coverımage = sec("coverImage").value; // Fotoğraf Linki
+  const bookname = sec("bookName").value; // Kitap adı
+  const authorname = sec("authorName").value; // Yazar adı
+  const booksubject = sec("bookSubject").value; // Kitap Konusu
+  const publicationyear = sec("publicationYear").value; // Kitap Basım Yılı
+  const coverımage = sec("linkAddress").value; // Fotoğraf Linki
+
+  List.push({
+    id: List.length + 1,
+    kitabadi: bookname,
+    yazaradi: authorname,
+    konusu: booksubject,
+    basimyili: publicationyear,
+    link: coverımage,
+  });
+  getAll();
+  sec("formItem").reset();
 }
 
-
-
-
-
-
-function ekle() {
-    const url = document.getElementById("linkAddress").value;
-    document.getElementById("priview").setAttribute('src', url);
+function getAll() {
+  let html = "";
+  List.forEach(function (kitab) {
+    html +=
+      "<tr>" +
+      "<td>" +
+      kitab.kitabadi +
+      "</td>" +
+      "<td>" +
+      kitab.yazaradi +
+      "</td>" +
+      "<td>" +
+      kitab.konusu +
+      "</td>" +
+      "<td>" +
+      kitab.basimyili +
+      "</td>" +
+      "<td>" +
+      "<img class= 'w-50' src='" +
+      kitab.link + // ("<image src='+item.url+'"); // "<img src='" +(burada örnk olrk kitab.link yzr)" ' />"
+      "' />";
+    "</td>" +
+      "<td>" +
+      "<button class= 'btn btn-primary mr-1' onClick='elemanDüzenle(" +
+      kitab.id +
+      ")'>Düzenle</button>" +
+      "<button class= 'btn btn-primary ml-1 ' onClick='elemanSil(" +
+      kitab.id +
+      ")'>Sil</button>" +
+      "<td>" +
+      "<tr>";
+  });
+  sec("content").innerHTML = html;
 }
 
-"<image src='+item.url+'"
+function duzenle() {
+  const duzen = List.findIndex((list) => list.id == editItem.id);
+  // findindex içerisnde bulunan parametrenin dizi içerisinde sırasını veren metoda fındındex denir.
+  // findindex içerisine değişken tanımladık ve onu Id ile uyumlu hale getirdik.
+  List[duzen].kitabadi = sec("bookName").value;
+  List[duzen].yazaradi = sec("authorName").value;
+  List[duzen].konusu = sec("bookSubject").value;
+  List[duzen].basimyili = sec("publicationYear").value;
+  List[duzen].link = sec("linkAddress").value;
+}
+// Yukarıda Push ettiğimiz değerleri burda çağırıp İd le eşleştiriyoruz.
+getAll();
+editItem = null;
+sec("formItem").reset();
+
+function elemanDüzenle(kitabId) {
+  editItem = List.find((list) => list.id === kitabId);
+
+  // eleman düzenleyi onclik olarak tetiklediğimiz için burda tanımladık .
+  // find elemanın kendisini verir bize.
+
+  sec("bookName").value = editItem.kitabadi;
+  sec("authorName").value = editItem.yazaradi;
+  sec("bookSubject").value = editItem.konusu;
+  sec("publicationYear").value = editItem.basimyili;
+  sec("linkAddress").value = editItem.link;
+}
+
+// Burdaki mantık veriyi sağdan alıp sola çekiyoruz. Not : Çok Önemli.
+
+function elemanSil(kitabId) {
+  if (confirm("Elemanı Silmek İstedğinize Eminmisiniz )")) {
+    const elemanıSil = List.findIndex((kitab) => kitab.id == kitabId);
+    List.splice(elemanıSil, 1);
+    getAll();
+  }
+}
+// 'splice' metodu ile js'de dizilere eleman ekleyip silebiliyoruz.
+// 'confirm' Elemanı gerçekten silmek istediğimizi teyit etmek için kullandık.
+// findindex içerisnde bulunan parametrenin dizi içerisnde sırasını veren metoda fındındex denir
+
+sec("formItem").addEventListener(
+  "submit",
+  () => {
+    if (parseInt(editItem.id) > 0) {
+      // parseInt veri türü ne olursa türünü number'a çevirir.
+      duzenle();
+    } else {
+      ekle();
+    }
+  },
+  false
+);
